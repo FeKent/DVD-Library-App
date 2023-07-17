e883b47f722547cddc32d0903bbd8724b88673ee
SnippyRex <kentf022@gmail.com>
2023-07-17 18:48:39 +0100

    Optimize Imports

--- app/src/main/java/com/example/dvdlibrary/MainActivity.kt
+++ app/src/main/java/com/example/dvdlibrary/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.dvdlibrary
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,7 +22,6 @@ import com.example.dvdlibrary.Screen.Intro
 import com.example.dvdlibrary.composables.AddScreen
 import com.example.dvdlibrary.composables.FilmScreen
 import com.example.dvdlibrary.composables.IntroScreen
-import com.example.dvdlibrary.composables.MainViewModel
 import com.example.dvdlibrary.data.DvdAppDatabase
 import com.example.dvdlibrary.data.Film
 import com.example.dvdlibrary.ui.theme.DVDLibraryTheme
--- app/src/main/java/com/example/dvdlibrary/composables/DeleteAlertDialog.kt
+++ app/src/main/java/com/example/dvdlibrary/composables/DeleteAlertDialog.kt
@@ -1,16 +1,11 @@
 package com.example.dvdlibrary.composables
 
-import android.app.AlertDialog
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun DeleteAlertDialog(
