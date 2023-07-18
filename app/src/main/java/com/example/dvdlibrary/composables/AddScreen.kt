f88bd03a1de31c30911ee89ed73a2be796735709
SnippyRex <kentf022@gmail.com>
2023-07-18 22:18:44 +0100

    Optimize Imports

--- app/src/main/java/com/example/dvdlibrary/composables/AddScreen.kt
+++ app/src/main/java/com/example/dvdlibrary/composables/AddScreen.kt
@@ -1,6 +1,5 @@
 package com.example.dvdlibrary.composables
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,7 +22,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -58,7 +56,7 @@ fun AddScreen(onFilmEntered: (Film) -> Unit, backButton: () -> Unit, modifier: M
         AddGenreField(selectedItem = genre, onGenreSelected = { genre = it })
         Spacer(modifier = Modifier.padding(24.dp))
 
-        Row() {
+        Row {
             BackButton {
                 backButton()
             }
