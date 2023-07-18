c63931b422a5464fca5f8bfd6f3393f994555fc1
SnippyRex <kentf022@gmail.com>
2023-07-18 15:44:39 +0100

    Add "Historical" Genre and alphabetised the Genre enum class

--- app/src/main/java/com/example/dvdlibrary/data/Genre.kt
+++ app/src/main/java/com/example/dvdlibrary/data/Genre.kt
@@ -9,15 +9,16 @@ enum class Genre(val printName: String, @DrawableRes val icon: Int) {
     Comedy("Comedy", R.drawable.genre_comedy),
     Drama("Drama", R.drawable.genre_drama),
     Fantasy("Fantasy", R.drawable.genre_fantasy),
+    Historical("Historical", R.drawable.genre_historical),
     Horror("Horror", R.drawable.genre_horror),
+    Lgbt("LGBT+", R.drawable.genre_lgbt),
     Musical("Musical", R.drawable.genre_musical),
     Mystery("Mystery", R.drawable.genre_mystery),
+    Parody("Parody", R.drawable.genre_parody),
     Romance("Romance", R.drawable.genre_romance),
     SciFi("Sci-Fi", R.drawable.genre_scifi),
     Sports("Sports", R.drawable.genre_sports),
     Thriller("Thriller", R.drawable.genre_thriller),
     Western("Western", R.drawable.genre_western),
-    Parody("Parody", R.drawable.genre_parody),
-    Zombie("Zombie", R.drawable.genre_zombie),
-    Lgbt("LGBT+", R.drawable.genre_lgbt)
+    Zombie("Zombie", R.drawable.genre_zombie)
 }
