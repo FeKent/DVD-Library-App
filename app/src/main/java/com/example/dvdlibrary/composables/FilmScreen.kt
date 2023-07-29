58f23fd496d140fa1e0a498d4b8d33f4a7f216b6
SnippyRex <kentf022@gmail.com>
2023-07-29 15:46:03 +0100

    Add error message to Film Screen if the poster_path value is empty

--- app/src/main/java/com/example/dvdlibrary/composables/FilmScreen.kt
+++ app/src/main/java/com/example/dvdlibrary/composables/FilmScreen.kt
@@ -2,15 +2,19 @@ package com.example.dvdlibrary.composables
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -50,14 +54,32 @@ fun FilmScreen(film: Film, onReturnTap: () -> Unit, modifier: Modifier = Modifie
             Text(text = "Runtime:", fontSize = 13.sp, fontStyle = FontStyle.Italic)
             Text(text = "${film.runtime} min", fontWeight = FontWeight.SemiBold)
         }
-        AsyncImage(
-            model = "https://image.tmdb.org/t/p/original${film.poster_path}",
-            contentDescription = film.description,
-            modifier = modifier
-                .fillMaxWidth()
-                .height(600.dp)
-                .width(300.dp)
-        )
+
+        if (film.poster_path.isNotEmpty()) {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/original${film.poster_path}",
+                contentDescription = film.description,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(600.dp)
+                    .width(300.dp)
+            )
+        } else {
+            Text(
+                text = "Poster Could Not Load",
+                fontSize = 13.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(600.dp)
+                    .wrapContentHeight(Alignment.CenterVertically)
+                    .width(300.dp)
+            )
+        }
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly
