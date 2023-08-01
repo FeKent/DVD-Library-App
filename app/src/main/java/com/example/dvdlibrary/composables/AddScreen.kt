8c98c32974a1e36c8a38f2e06af15db5ba679faf
SnippyRex <kentf022@gmail.com>
2023-08-01 02:32:49 +0100

    Remove unused code

--- app/src/main/java/com/example/dvdlibrary/composables/AddScreen.kt
+++ app/src/main/java/com/example/dvdlibrary/composables/AddScreen.kt
@@ -1,15 +1,12 @@
 package com.example.dvdlibrary.composables
 
-import android.icu.text.CaseMap.Title
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -29,7 +26,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -58,7 +54,6 @@ fun AddScreen(
     val validationLabel = remember { mutableStateOf("") }
 
     val posterScope = CoroutineScope(Dispatchers.Main)
-    val mContext = LocalContext.current
     val apiKey =
         "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjJiN2NiZTdlMzc4NGQwN2U1Y2I3NDUxOTFmODYxZSIsInN1YiI6IjY0YTBhYTU2ODFkYTM5MDE0ZDQ5ZDM0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P_rg4mB-Xp5yXhSp9J_qSkf-9aZ134SIzEZz_HlsQj0"
 
