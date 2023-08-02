5db90b54026ee43e770004b28b7099922a4ba5aa
SnippyRex <kentf022@gmail.com>
2023-08-02 01:02:24 +0100

    Setup compose navigation

--- app/src/main/java/com/example/dvdlibrary/MainActivity.kt
+++ app/src/main/java/com/example/dvdlibrary/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.dvdlibrary
 
 import android.os.Bundle
+import android.telecom.Call.Details
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -16,6 +18,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import com.example.dvdlibrary.Screen.Add
 import com.example.dvdlibrary.Screen.Details
@@ -51,9 +56,68 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//@Composable
+//fun DvdApp() {
+//    var currentScreen by remember { mutableStateOf<Screen>(Intro) }
+//    val appContext = LocalContext.current.applicationContext
+//    val database by remember {
+//        mutableStateOf(
+//            Room.databaseBuilder(
+//                appContext,
+//                DvdAppDatabase::class.java,
+//                "database-name"
+//            ).build()
+//        )
+//    }
+//    val coroutineScope = rememberCoroutineScope()
+//    val films by database.filmsDao().allFilms().collectAsStateWithLifecycle(emptyList())
+//    val showDialogState = remember { mutableStateOf(false) }
+//
+//
+//    when (val cs = currentScreen) {
+//        Intro -> IntroScreen(
+//            films = films.sortedBy(Film::title),
+//            onAddBtnTap = { currentScreen = Add },
+//            onFilmTap = { film -> currentScreen = Details(film) },
+//            removeFilm = { coroutineScope.launch { database.filmsDao().delete(it) } }
+//        )
+//
+//        Add -> AddScreen(onFilmEntered = { newFilm ->
+//
+//            val isFilmDuplicate = existingFilm(films, newFilm)
+//            if (!isFilmDuplicate) {
+//                coroutineScope.launch {
+//                    database.filmsDao().insertFilm(newFilm)
+//                    currentScreen = Intro
+//                }
+//            } else {
+//                showDialogState.value = true
+//            }
+//        }, backButton = { currentScreen = Intro }, showDialogState = showDialogState
+//        )
+//
+//        is Details -> FilmScreen(cs.film, onReturnTap = { currentScreen = Intro })
+//    }
+//}
+//
+//fun existingFilm(filmList: List<Film>, newFilm: Film): Boolean {
+//    return filmList.any { existingFilm ->
+//        existingFilm.title == newFilm.title && existingFilm.year == newFilm.year
+//    }
+//}
+//
+//
+//sealed class Screen {
+//    object Intro : Screen()
+//    object Add : Screen()
+//    class Details(val film: Film) : Screen()
+//}
+
+
 @Composable
 fun DvdApp() {
-    var currentScreen by remember { mutableStateOf<Screen>(Intro) }
+
+    var currentScreen by remember { mutableStateOf("intro") }
     val appContext = LocalContext.current.applicationContext
     val database by remember {
         mutableStateOf(
@@ -64,47 +128,45 @@ fun DvdApp() {
             ).build()
         )
     }
-    val coroutineScope = rememberCoroutineScope()
     val films by database.filmsDao().allFilms().collectAsStateWithLifecycle(emptyList())
+    val coroutineScope = rememberCoroutineScope()
     val showDialogState = remember { mutableStateOf(false) }
+    val navController = rememberNavController()
 
+    NavHost(navController = navController, startDestination = "intro") {
+        composable("intro") {
+            IntroScreen(
+                films = films.sortedBy(Film::title),
+                onAddBtnTap = { currentScreen = "add" },
+                onFilmTap = { film -> currentScreen = Details(film) },
+                removeFilm = { coroutineScope.launch { database.filmsDao().delete(it) } }
+            )
+        }
+        composable("add") {
+            AddScreen(onFilmEntered = { newFilm ->
 
-    when (val cs = currentScreen) {
-        Intro -> IntroScreen(
-            films = films.sortedBy(Film::title),
-            onAddBtnTap = { currentScreen = Add },
-            onFilmTap = { film -> currentScreen = Details(film) },
-            removeFilm = { coroutineScope.launch { database.filmsDao().delete(it) } }
-        )
-
-        Add -> AddScreen(onFilmEntered = { newFilm ->
-
-            val isFilmDuplicate = existingFilm(films, newFilm)
-            if (!isFilmDuplicate) {
-                coroutineScope.launch {
-                    database.filmsDao().insertFilm(newFilm)
-                    currentScreen = Intro
+                val isFilmDuplicate = existingFilm(films, newFilm)
+                if (!isFilmDuplicate) {
+                    coroutineScope.launch {
+                        database.filmsDao().insertFilm(newFilm)
+                        currentScreen = "intro"
+                    }
+                } else {
+                    showDialogState.value = true
                 }
-            } else {
-                showDialogState.value = true
-            }
-        }, backButton = { currentScreen = Intro }, showDialogState = showDialogState
-        )
+            }, backButton = { currentScreen = "intro" }, showDialogState = showDialogState
+            )
+        }
+        composable("Details") {
+            FilmScreen(cs.film, onReturnTap = { currentScreen = Intro })
 
-        is Details -> FilmScreen(cs.film, onReturnTap = { currentScreen = Intro })
+        }
     }
 }
+
 
 fun existingFilm(filmList: List<Film>, newFilm: Film): Boolean {
     return filmList.any { existingFilm ->
         existingFilm.title == newFilm.title && existingFilm.year == newFilm.year
     }
 }
-
-
-sealed class Screen {
-    object Intro : Screen()
-    object Add : Screen()
-    class Details(val film: Film) : Screen()
-}
-
