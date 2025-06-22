80268dbad5298e80185b6ae0023147769ffdcb71
FeKent <kentf022@gmail.com>
2025-06-22 13:19:20 +0100

    fix: align data class field names with API

--- app/src/main/java/com/example/dvdlibrary/networking/TmdbMovieCastSearchResults.kt
+++ app/src/main/java/com/example/dvdlibrary/networking/TmdbMovieCastSearchResults.kt
@@ -1,5 +1,5 @@
 package com.example.dvdlibrary.networking
 
 data class TmdbMovieCastSearchResults(
-    val results: List<TmdbMovieCastSearchResult>
+    val cast: List<TmdbMovieCastSearchResult>
 )
--- app/src/main/java/com/example/dvdlibrary/networking/TmdbMoviePosterSearchResult.kt
+++ app/src/main/java/com/example/dvdlibrary/networking/TmdbMoviePosterSearchResult.kt
@@ -1,20 +1,13 @@
 package com.example.dvdlibrary.networking
 
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import retrofit2.Retrofit
-import retrofit2.http.GET
-
 data class TmdbMoviePosterSearchResult(
     val title: String?,
     val poster_path: String?,
     val overview: String?,
     val release_date: String?,
     val first_airdate: String?,
-    val media_type: String?
+    val media_type: String?,
+    val id: Int?
 )
 
 
